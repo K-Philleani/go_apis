e81df363552b68a2a615fa237e2a2195814e46f1
K-Philleani <964202623@qq.com>
2020-09-28 15:13:24 +0800

    增加跨域

--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	_ "go_apis/conn"
 	routers "go_apis/router"
-	"net/http"
 )
 
 func main() {
 	router := routers.Routers()
-	router.Use(cors())
 	router.Run(":9090")
 }
 
-func cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		method := c.Request.Method
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, x-www-form-urlencoded")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-		}
-
-		c.Next()
-	}
-}
--- router/router.go
+++ router/router.go
@@ -3,10 +3,12 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"go_apis/apis"
+	"net/http"
 )
 
 func Routers() *gin.Engine{
 	router := gin.Default()
+	router.Use(cors())
 
 	account := router.Group("/")
 	{
@@ -16,4 +18,20 @@ func Routers() *gin.Engine{
 	}
 
 	return router
+}
+
+func cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, x-www-form-urlencoded")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+
+		c.Next()
+	}
 }
